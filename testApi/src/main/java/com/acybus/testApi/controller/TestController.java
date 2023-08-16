6d5a7361af1119d4ccd81bd776e7fc8be558f4d1
Jagdish1988 <34127027+Jagdish1988@users.noreply.github.com>
2023-08-16 12:53:02 +0530

    yes
    My TestApi project has register with gitb

---
+++ testApi/src/main/java/com/acybus/testApi/controller/TestController.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.acybus.testApi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ */
+@RestController
+public class TestController {
+	
+	@GetMapping(path = "/")
+	public String getHome() {
+		return " Hi Welcome to you in Acybus family.";
+	}
+}
---
+++ testApi/src/test/java/com/acybus/testApi/TestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acybus.testApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
